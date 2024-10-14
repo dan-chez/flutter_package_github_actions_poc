b5087a296aef87ce46a54220e93c0b6641b7de15
Daniel Sanchez <danielmsa96@gmail.com>
2024-10-14 10:58:35 -0500

    Fix export

--- lib/flutter_package_github_actions_poc.dart
+++ lib/flutter_package_github_actions_poc.dart
@@ -1,8 +1,3 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
 library;
 
-export 'src/flutter_package_github_actions_poc_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
+export 'src/components/cm_page_title.dart';
